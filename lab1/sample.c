e80511c3d3f5cd8bf0d6c90e509f72ac9791d446
Yan Luo <yan_luo@uml.edu>
2014-09-05 19:07:31 -0400

    update lab1/sample.c

--- lab1/sample.c
+++ lab1/sample.c
@@ -1,1 +1,45 @@
-/* * File:   sample.c *  * simple PIC program example * for UMass Lowell 16.480/552 * * Author: Roy * * Created on 2014/9/3 */#include <pic16f688.h>void delay(unsigned  dly){    unsigned int temp;    for(;dly>0;dly--)        for(temp=1000;temp>0;temp--);}void main(int argc, char **argv) {    TRISCbits.TRISC0 = 0;      //set RC0 as output    while(1)    {        PORTCbits.RC0 = 1;     //RC0 is high level        delay(100);        PORTCbits.RC0 = 0; 	//RC0 is low level	  delay(100);       }}
+imple PIC program example
+ * for UMass Lowell 16.480/552
+ * 
+ * Author: Roy
+ *
+ * Created on 2014/9/5
+ *
+ * Reference: Start from PIC16F688 Data Sheet Page 67
+ */
+
+
+#include <pic16f688.h>
+
+//initialize ADC
+void adc_init()
+{
+    /*
+      1.turn off comparators
+      2.select pin as analog input
+      3.set A/D control registers
+   */
+} 
+
+//read ADC value
+void adc_read()
+{
+   /*
+     1.set ADC status bit to start a cycle and wait until the process is completed
+     2.clear ADC interrupt flag bit
+     3.read the value and do the comparation
+   */
+    
+} 
+ 
+// Main program
+void main (void)
+{
+    adc_init();
+    while(1)
+    {
+        adc_read();
+    }
+}
+
+
